3c6fa8a0c2e0fcd367b5ad615f989773b40eea63
vickiscode <mailtovickikumar@gmail.com>
2025-09-03 23:43:53 +0530

    In development mode

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import connectDB from './configs/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
 
 // App Config
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 const app = express();
 await connectDB()
 
